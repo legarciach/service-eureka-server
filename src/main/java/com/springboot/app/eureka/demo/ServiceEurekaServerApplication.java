f14dcfd081354d5628f7ad71f9b2bda2b39dcd57
Enrique Garcia <enrique.escom@gmail.com>
2020-09-25 22:16:11 -0500

    Eureka server

---
+++ src/main/java/com/springboot/app/eureka/demo/ServiceEurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.app.eureka.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceEurekaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springboot/app/eureka/demo/ServiceEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.app.eureka.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
